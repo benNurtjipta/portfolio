29b6c7878a0b0d2f57ec0fda04c36977ecf0a896
benNurtjipta <ben@nurtjipta.de>
2025-05-27 20:04:39 +0200

    content done

---
+++ src/components/Projects.jsx
@@ -0,0 +1,42 @@
+const Projects = () => {
+  const projects = [
+    {
+      title: "leafio",
+      description: "Project: A marketplace to buy and sell plants.",
+      link: "https://leafio.vercel.app/",
+    },
+    {
+      title: "808",
+      description: "Simple stepsequencer using Web Audio API.",
+      link: "https://808-rose.vercel.app/",
+    },
+    {
+      title: "run-in-dir",
+      description: "VS Code extension to run a script from status bar.",
+      link: "https://github.com/benNurtjipta/run-in-dir",
+    },
+    {
+      title: "vscrx",
+      description: "Android app to remote control VS Code .",
+      link: "https://github.com/benNurtjipta/vscrx",
+    },
+  ];
+
+  return (
+    <div className="projects-list">
+      {projects.map((project, idx) => (
+        <div key={idx} className="project-item">
+          <h5>{project.title}</h5>
+          <p>{project.description}</p>
+          <a href={project.link} target="_blank" rel="noopener noreferrer">
+            {project.link.includes("github")
+              ? "View on GitHub"
+              : "Visit Website"}
+          </a>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Projects;
--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,3 +1,5 @@
+import Projects from "./Projects";
+
 const Tech = ({
   setShowExp,
   setShowPicture,
@@ -19,18 +21,19 @@ const Tech = ({
       className={`left-aside ${showExp ? "fade-in-about" : "hidden-about"}`}
     >
       <h4>STACK</h4>
-      <p>I create web applications with HTML, CSS, JavaScript, and React.</p>
       <p>
-        On the backend side, I work with Node.js and Express, and manage my code
-        using Git. Recently, I’ve also started exploring React Native to build
-        mobile apps.
+        I create web applications with HTML, CSS, JavaScript, and React. On the
+        backend side, I work with Node.js and Express, and manage my code using
+        Git. Recently, I’ve also started exploring React Native to build mobile
+        apps.
       </p>
-      <p>Whatever the project needs, I’m quick to adapt.</p>
+      <h4>PROJECTS</h4>
+      <Projects />
       <button
         onClick={resetAll}
         className={`nav-button exp ${showExp ? "fade-in" : "hidden"}`}
       >
-        reset website
+        reset
       </button>
     </aside>
   );
