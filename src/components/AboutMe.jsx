92572a98588ba90b7844377a1e92b7f24e55ec75
benNurtjipta <ben@nurtjipta.de>
2025-05-30 16:12:24 +0200

    resp img smaller

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -2,66 +2,69 @@ import React, { forwardRef } from "react";
 
 const AboutMe = forwardRef(({ showAbout, onExpClick }, ref) => {
   return (
-    <aside
-      ref={ref}
-      className={`right-aside ${showAbout ? "fade-in-about" : "hidden-about"}`}
-    >
-      <h4>ABOUT ME</h4>
-      <p>
-        Hi, I'm Ben — a web developer in the making. I’m finishing my studies at
-        DCI Digital Career Institute (Nov 2025) and ready to take on new
-        challenges.
-      </p>
-      <p>
-        With a background in video editing and team leadership, I bring creative
-        thinking, strong communication, and a collaborative mindset to the
-        table.
-      </p>
-      <p>
-        I'm passionate about music, curious about new tech, love good food, and
-        always eager to learn something new.
-      </p>
-
-      <pre className="code-block">
-        <code>
-          <span className="const-span">const</span> contactMe = {"{\n"}
-          {"  "}email:{" "}
-          <a
-            className={`${showAbout ? "fade-in" : "hidden"}`}
-            href="mailto:b.nurtjipta@gmail.com"
-          >
-            "b.nurtjipta@gmail.com"
-          </a>
-          ,{"\n"}
-          {"  "}github:{" "}
-          <a
-            className={`${showAbout ? "fade-in" : "hidden"}`}
-            href="https://github.com/benNurtjipta"
-            target="_blank"
-          >
-            "benNurtjipta"
-          </a>
-          ,{"\n"}
-          {"  "}linkedin:{" "}
-          <a
-            className={`${showAbout ? "fade-in" : "hidden"}`}
-            href="https://linkedin.com/in/ben-nurtjipta"
-            target="_blank"
-          >
-            "ben-nurtjipta"
-          </a>
-          {"\n"}
-          {"}"}
-        </code>
-      </pre>
-
-      <button
-        onClick={onExpClick}
-        className={`nav-button exp ${showAbout ? "fade-in" : "hidden"}`}
+    <div className="outer-rim" ref={ref}>
+      <aside
+        className={`right-aside ${
+          showAbout ? "fade-in-about" : "hidden-about"
+        }`}
       >
-        experience
-      </button>
-    </aside>
+        <h4>ABOUT ME</h4>
+        <p>
+          Hi, I'm Ben — a web developer in the making. I’m finishing my studies
+          at DCI Digital Career Institute (Nov 2025) and ready to take on new
+          challenges.
+        </p>
+        <p>
+          With a background in video editing and team leadership, I bring
+          creative thinking, strong communication, and a collaborative mindset
+          to the table.
+        </p>
+        <p>
+          I'm passionate about music, curious about new tech, love good food,
+          and always eager to learn something new.
+        </p>
+
+        <pre className="code-block">
+          <code>
+            <span className="const-span">const</span> contactMe = {"{\n"}
+            {"  "}email:{" "}
+            <a
+              className={`${showAbout ? "fade-in" : "hidden"}`}
+              href="mailto:b.nurtjipta@gmail.com"
+            >
+              "b.nurtjipta@gmail.com"
+            </a>
+            ,{"\n"}
+            {"  "}github:{" "}
+            <a
+              className={`${showAbout ? "fade-in" : "hidden"}`}
+              href="https://github.com/benNurtjipta"
+              target="_blank"
+            >
+              "benNurtjipta"
+            </a>
+            ,{"\n"}
+            {"  "}linkedin:{" "}
+            <a
+              className={`${showAbout ? "fade-in" : "hidden"}`}
+              href="https://linkedin.com/in/ben-nurtjipta"
+              target="_blank"
+            >
+              "ben-nurtjipta"
+            </a>
+            {"\n"}
+            {"}"}
+          </code>
+        </pre>
+
+        <button
+          onClick={onExpClick}
+          className={`nav-button exp ${showAbout ? "fade-in" : "hidden"}`}
+        >
+          experience
+        </button>
+      </aside>
+    </div>
   );
 });
 export default AboutMe;
--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -16,36 +16,38 @@ const Tech = forwardRef(
     ref
   ) => {
     return (
-      <aside
-        ref={ref}
-        className={`left-aside ${showExp ? "fade-in-about" : "hidden-about"}`}
-      >
-        <h4>STACK</h4>
-        <p>
-          I create web applications with HTML, CSS, JavaScript, and React. On
-          the backend side, I work with Node.js and Express, and manage my code
-          using Git. Recently, I’ve also started exploring React Native to build
-          mobile apps. <br />
-          Whatever the project needs, I’m quick to adapt.
-        </p>
-        <h4>PROJECTS</h4>
-        <Projects />
-        <div className="double-buttons exp">
-          <button
-            onClick={onReset}
-            className={`nav-button  ${showExp ? "fade-in" : "hidden"}`}
-          >
-            reset
-          </button>
-          <a
-            href="/cv.pdf"
-            download="Ben_Nurtjipta_CV.pdf"
-            className={`nav-button ${showExp ? "fade-in" : "hidden"}`}
-          >
-            cv
-          </a>
-        </div>
-      </aside>
+      <div className="outer-rim" ref={ref}>
+        <aside
+          ref={ref}
+          className={`left-aside ${showExp ? "fade-in-about" : "hidden-about"}`}
+        >
+          <h4>STACK</h4>
+          <p>
+            I create web applications with HTML, CSS, JavaScript, and React. On
+            the backend side, I work with Node.js and Express, and manage my
+            code using Git. Recently, I’ve also started exploring React Native
+            to build mobile apps. <br />
+            Whatever the project needs, I’m quick to adapt.
+          </p>
+          <h4>PROJECTS</h4>
+          <Projects />
+          <div className="double-buttons exp">
+            <button
+              onClick={onReset}
+              className={`nav-button  ${showExp ? "fade-in" : "hidden"}`}
+            >
+              reset
+            </button>
+            <a
+              href="/cv.pdf"
+              download="Ben_Nurtjipta_CV.pdf"
+              className={`nav-button ${showExp ? "fade-in" : "hidden"}`}
+            >
+              cv
+            </a>
+          </div>
+        </aside>
+      </div>
     );
   }
 );
